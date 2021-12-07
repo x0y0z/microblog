2f9ec12d6ab0853b353db8e915af03f66eee609c
x0y0z <38836790+x0y0z@users.noreply.github.com>
2021-12-07 22:38:53 +0000

    Customised error email subjects

--- app/__init__.py
+++ app/__init__.py
@@ -53,6 +53,8 @@ def create_app(config_class=Config):
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
+    import socket
+
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
             auth = None
@@ -64,8 +66,8 @@ def create_app(config_class=Config):
                 secure = ()
             mail_handler = SMTPHandler(
                 mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-                toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+                fromaddr=app.config['ADMINS'][0],
+                toaddrs=app.config['ADMINS'], subject=f"Microblog Failure ({socket.gethostname()})",
                 credentials=auth, secure=secure)
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
